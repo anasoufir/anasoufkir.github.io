867d87da00edc6117426ea3dc840fa3dac8594c8
anasoufir <etu_oufkir.anas@emsi-edu.ma>
2023-05-23 20:57:36 +0100

    good

--- src/App.js
+++ src/App.js
@@ -1,20 +1,9 @@
-import React from 'react';
-import Analytics from './components/Analytics';
-import Cards from './components/Cards';
-import Footer from './components/Footer';
-import Hero from './components/Hero';
-import Navbar from './components/Navbar';
-import Newsletter from './components/Newsletter';
-
+import React from "react";
+import Home from "./pages/Home";
 function App() {
   return (
     <div>
-      <Navbar />
-      <Hero />
-      <Analytics />
-      <Newsletter />
-      <Cards />
-      <Footer />
+      <Home />
     </div>
   );
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,20 @@
+import Analytics from "./components/Analytics";
+import Cards from "./components/Cards";
+import Footer from "./components/Footer";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+import Newsletter from "./components/Newsletter";
+
+const Home = () => {
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+      <Analytics />
+      <Newsletter />
+      <Cards />
+      <Footer />
+    </div>
+  );
+};
+export default Home;
